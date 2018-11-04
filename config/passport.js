21f52679597cbc55de12c52711ea824682198cf8
unknown <ferdian89>
2018-11-04 13:30:56 +0700

    add signin route

--- config/passport.js
+++ config/passport.js
@@ -47,3 +47,34 @@ passport.use('local.signup', new LocalStrategy ({
     });
   });
 }));
+
+
+passport.use('local.signin', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+  req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
+  req.checkBody('password', 'Invalid Password').notEmpty();
+  req.checkBody('password', "Password's length at least 5").isLength({min:4});
+  var errors = req.validationErrors();
+  if (errors) {
+    var messages = [];
+    errors.forEach(function(error) {
+      messages.push(error.msg);
+    });
+    return done(null, false, req.flash('error', messages));
+  }
+  User.findOne({'email': email}, function(err, user) {
+    if(err) {
+      return done(err);
+    }
+    if(!user) {
+      return done(null, false, {message:'user not found'})
+    }
+    if(!user.validPassword(password)) {
+      return done(null, false, {message:'wrong password'})
+    }
+    return done(null, user);
+  });
+}));
