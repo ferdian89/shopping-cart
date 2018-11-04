231c433a3181fdb6a1a0485c58d33401fe70c8e3
unknown <ferdian89>
2018-11-04 12:41:09 +0700

    add bcrypt-nodejs

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
-const bcrypts = require('bcryptjs');
+
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
@@ -14,11 +14,11 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use('local.signup', new LocalStrategy ({
-  userNameField: 'email',
+  usernameField: 'email',
   passwordField: 'password',
-  passwordReqToCallback: true
+  passReqToCallback: true
 }, function(req, email, password, done) {
-  user.findOne({'email': email}, function(err, user) {
+  User.findOne({'email': email}, function(err, user) {
     if(err) {
       return done(err);
     }
@@ -27,21 +27,12 @@ passport.use('local.signup', new LocalStrategy ({
     }
     var newUser = new User();
     newUser.email = email;
-    newUser.password = password;
-  });
-  bcrypt.gensalt(10, function(err, salt) {
-    bcrypt.hash(newUser.password, salt, function(err, hash) {
-      if(err) {
+    newUser.password = newUser.encryptPassword(password);
+    newUser.save(function(err, result) {
+      if (err) {
         return done(err);
       }
-      newUser.email = email;
-      newUser.password = hash;
-      newUser.save(function(err, result) {
-        if (err) {
-          return done(err);
-        }
-          return done(null, newUser);
-      });
+      return done(null, newUser);
     });
   });
 }));
